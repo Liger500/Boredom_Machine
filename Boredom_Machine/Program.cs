0c174c199e9fe88d000a83c1687c1825cf0f4e51
Liger500 <liger500@gmail.com>
2017-08-18 11:57:13 -0400

    First Commit

--- Boredom_Machine/Program.cs
+++ Boredom_Machine/Program.cs
@@ -10,6 +10,34 @@ namespace Boredom_Machine
     {
         static void Main(string[] args)
         {
+            int c;
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1) Video Games");
+            Console.WriteLine("2) Movies");
+            Console.WriteLine("3) TV");
+            Console.WriteLine("4) Random");
+
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (c == 1)
+            {
+                Console.WriteLine("");
+            }
+            if (c == 2)
+            {
+                Console.WriteLine("");
+            }
+            if (c == 3)
+            {
+                Console.WriteLine("");
+            }
+            if (c == 4)
+            {
+                Console.WriteLine("");
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
